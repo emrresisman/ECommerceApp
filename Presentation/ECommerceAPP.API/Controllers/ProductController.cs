354d1ed4bdc0c270900f5259c002d677fcdd5ee1
Emre Şişman <emrre98@outlook.com>
2022-07-15 21:05:51 +0300

    as no tracking perfomence optimization

--- Presentation/ECommerceAPP.API/Controllers/ProductController.cs
+++ Presentation/ECommerceAPP.API/Controllers/ProductController.cs
@@ -20,13 +20,17 @@ namespace ECommerceAPP.API.Controllers
         [HttpGet]
         public async Task Get()
         {
-            await _productWriteRepository.AddRangeAsync(new()
-            {
-                new(){Id=Guid.NewGuid(),Name="Product 1",CreatedDate=DateTime.UtcNow,Price=100,Stock=10},
-                new(){Id=Guid.NewGuid(),Name="Product 2",CreatedDate=DateTime.UtcNow,Price=200,Stock=20},
-                new(){Id=Guid.NewGuid(),Name="Product 3",CreatedDate=DateTime.UtcNow,Price=300,Stock=30},
-            });
-            await _productWriteRepository.SaveAsync();
+            //await _productWriteRepository.AddRangeAsync(new()
+            //{
+            //    new(){Id=Guid.NewGuid(),Name="Product 1",CreatedDate=DateTime.UtcNow,Price=100,Stock=10},
+            //    new(){Id=Guid.NewGuid(),Name="Product 2",CreatedDate=DateTime.UtcNow,Price=200,Stock=20},
+            //    new(){Id=Guid.NewGuid(),Name="Product 3",CreatedDate=DateTime.UtcNow,Price=300,Stock=30},
+            //});
+            //await _productWriteRepository.SaveAsync();
+
+            Product p = await _productReadRepository.GetByIdAsync("53d7241b-2801-489b-a3c6-c99cf11b24ea",false);
+            p.Name = "Product 13";
+            _productWriteRepository.SaveAsync();
         }
         [HttpGet("{id}")]
         public async Task<IActionResult> Get(string id)
